feb5f44cfc585eceed447b227a96970d6ae258c1
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-18 18:11:06 -0700

    Third commit

--- comsc-210-lab09.cpp
+++ comsc-210-lab09.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 const int SIZE = 30;
 
+//Function Prototypes
+void size(array<int,SIZE> points);
+
 int main(){
 
     array<int,SIZE> points;
@@ -19,5 +22,16 @@ int main(){
         return 1;
     }
 
+    for(int i = 0; i < SIZE; i++){
+        inputFile >> points[i];
+    }
+
+    inputFile.close();
+
+
+}
+
+void size(array<int,SIZE> points){
+    cout << "Here are Stephen Curry's recent 30 games: ";
     
 }
